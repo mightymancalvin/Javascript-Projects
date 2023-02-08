d090a2f3886c1153d36e2bc968e4fb2ad4e7fcc0
mightymancalvin <mightymanmensah@gmail.com>
2023-02-08 18:38:09 +0000

    modifications to scripts

---
+++ practice/render-error/index.js
@@ -0,0 +1,11 @@
+// let errorLine = document.getElementById('error-el')
+
+// function error_msg() {
+//     errorLine.innerText("Something went wrong, please try again")
+// }
+
+
+// error_msg()
+
+alpha = document.getElementById('error-el')
+console.log(alpha)
