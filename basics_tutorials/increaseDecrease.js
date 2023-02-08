5203ece96bb4620e6f7bc0475956046e58184ecb
mightymancalvin <mightymanmensah@gmail.com>
2023-02-08 08:51:54 +0000

    added a new script increaseDecrease

---
+++ basics_tutorials/increaseDecrease.js
@@ -0,0 +1,15 @@
+// increase and deacrese an initial value 
+
+let bonusPoints = 50
+
+// increase to 100
+bonusPoints = bonusPoints * 2
+console.log(bonusPoints)
+
+// decrease to 25 
+bonusPoints = bonusPoints * 0.25
+console.log(bonusPoints)
+
+// increase to 70 
+bonusPoints = bonusPoints + 45
+console.log(bonusPoints)
