78da9e98172889352536e4c1fc7b51141be74ff7
mightymancalvin <mightymanmensah@gmail.com>
2023-03-09 10:41:22 +0000

    Update

---
+++ Meta-React-Native-Specialization/Programming_in_Javascript/advanced_operators.js
@@ -0,0 +1,33 @@
+// Task 1 
+var score = 8;
+console.log("Mi-Level-Skills:", (0<score) && (score<10));
+
+// Task 2
+var timeRemaining = 0;
+var energy = 10;
+console.log("Game Over: ", (timeRemaining ==0) || (energy ==0))
+
+// Task 3
+var num1 = 2, num2 = 5;
+var test1 = num1 % 2, test2 = num2 % 2;
+var result1 = test1 == 0, result2 = test2 == 0;
+
+console.log("Is", num1, "an even number?", result1);
+console.log("Is", num2, "an even number?", result2);
+
+// Task 4
+console.log(5 + 10);
+
+// Task 5
+var now = "Now in ";
+var three = 3;
+var d = "D!"
+
+console.log(now + three + d)
+
+// Task 6
+var counter = 0;
+counter += 5;
+counter += 3;
+
+console.log(counter)
