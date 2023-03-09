6094b31a312e622e707b4bc4b1ce907fe3f1bd81
mightymancalvin <mightymanmensah@gmail.com>
2023-03-09 14:23:39 +0000

    updates

---
+++ Meta-React-Native-Specialization/Programming_in_Javascript/loops.js
@@ -0,0 +1,34 @@
+// Task 1 
+for(i=1; i<=5; i++) {
+    console.log(i);
+}
+console.log("Counting completed!");
+
+// Task 2
+for(i=5; i>0; i--){
+    console.log(i);
+}
+console.log("Countdown Completed!");
+
+// Task 3
+var counter = 1;
+while (counter <=5) {
+    console.log(counter);
+    counter++;
+}
+console.log("Counting completed!")
+
+// Task 4
+var counter = 5;
+while (counter > 0){
+    console.log(counter);
+    counter--;
+}
+console.log("Countdown Finishes!")
+
+// Task 5
+var year = 2018;
+while (year < 2023){
+    console.log(year);
+    year++;
+}
